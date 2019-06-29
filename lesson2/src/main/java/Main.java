63c5c495cd9d6a71a4ba723bcfe05c6ffc26830a
mihassu <mihassu1@gmail.com>
2019-06-29 12:21:44 +0300

    ДЗ 2 Папунин М

--- lesson2/src/main/java/Main.java
+++ lesson2/src/main/java/Main.java
@@ -6,12 +6,24 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         MyArray<Integer> arr = new MyArray<>();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 10; i++) {
             arr.add(new Random().nextInt(1000));
         }
         arr.show();
-        //Arrays.sort();
-        //arr.sort(Comparator.comparingInt(o -> o));
+        arr.sort(Comparator.comparingInt(o -> o));
+        //ArrayUtility.sort(arr);
+        //ArrayUtility.insertSort(arr);
+        //ArrayUtility.selectSort(arr);
         arr.show();
+        arr.add(1, 55);
+        arr.show();
+        arr.remove(7);
+        arr.show();
+
+        System.out.println(ArrayUtility.binarySearch(arr, 3));
+        System.out.println(ArrayUtility.linearSearch(arr, 3));
+
+
+
     }
 }
--- lesson2/src/main/java/MyArray.java
+++ lesson2/src/main/java/MyArray.java
@@ -4,13 +4,13 @@ import java.util.Comparator;
 
 public class MyArray<T> {
 
-    private Integer[] data;
+    private Object[] data;
     private int size, capacity, q;
 
     public MyArray() {
-        size = 0;
+        size = 0; //количество находящихся в массиве элементов
         capacity = 1000;
-        data = new Integer[capacity];
+        data = new Object[capacity];
     }
 
     void getQ(){
@@ -25,7 +25,7 @@ public class MyArray<T> {
     }
 
     //вставка в конец массива
-    void add(Integer element){
+    void add(T element){
         if(size < capacity){
             data[size] = element;
             size++;
@@ -37,12 +37,12 @@ public class MyArray<T> {
         }
     }
 
-    void set(int index, Integer value){
+    void set(int index, T value){
         data[index] = value;
     }
 
     T get(int index){
-        return null;
+        return (T)data[index];
     }
 
     int size(){
@@ -51,7 +51,11 @@ public class MyArray<T> {
 
     //вставка по индексу
     void add(int index, T element){
-        //TODO
+        for (int i = size - 1; i >= index; i--) {
+            data[i + 1] = data[i];
+        }
+        data[index] = element;
+        size++;
     }
 
     //перевыделение памяти можно увеличить вместимость
@@ -59,7 +63,7 @@ public class MyArray<T> {
     //подумайте как лучше
     private void reallocate(){
         capacity *= 2;
-        Integer[] tmp = new Integer[capacity];
+        Object[] tmp = new Object[capacity];
         if (size >= 0) System.arraycopy(data, 0, tmp, 0, size);
         data = tmp;
     }
@@ -67,42 +71,61 @@ public class MyArray<T> {
     //удаление последнего
     boolean remove(){
         size--;
+        if (size < 0) {
+            size = 0;
+        }
         return false;
     }
     //удаление по индексу
     boolean remove(int index){
-        //TODO
+
+        for (int i = index; i < size - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        size--;
+        if (size < 0) {
+            size = 0;
+        }
         return false;
     }
 
     T [] toArray(){
-        //TODO
         Object[] tmp = new Object[size];
         System.arraycopy(data, 0, tmp, 0, size);
         return (T[]) tmp;
     }
 
-    void sort(){
-        int left = 0, right = size;
-        qs(data, left, right-1);
-    }
+//    void sort(Comparator<T> c){
+//        //int q = 0;
+//        for (int i = 0; i < size; i++) {
+//            //int swaps = 0;
+//            for (int j = 0; j < size-i-1; j++) {
+//                if(c.compare((T)data[j], (T)data[j+1]) > 0){
+//                    //swaps++;
+//                    Object tmp = data[j+1];
+//                    data[j+1] = data[j];
+//                    data[j] = tmp;
+//                } else continue;
+//                //q++;
+//            }
+//            //if(swaps == 0) break;
+//        }
+//    }
 
-    private void qs(Integer[] data, int left, int right) {
-        if(left < right){
-            int i = left, j = right;
-            int p = data[(i+j) / 2];
-            do{
-                while (data[i] < p) i++;
-                while (data[j] > p) j--;
-                if(i <= j){
-                    Integer tmp = data[i];
-                    data[i] = data[j];
-                    data[j] = tmp;
-                    i++; j--;
-                }
-            }while (i <= j);
-            qs(data, left, j);
-            qs(data, i, right);
+    void sort(Comparator<T> c) {
+        Object temp;
+        int len = size - 1;
+
+        while (len > 0) {
+            for (int i = 1; i <= len; i++) {
+                if (c.compare((T)data[i - 1], (T)data[i]) > 0) {
+                    temp = data[i - 1];
+                    data[i - 1] = data[i];
+                    data[i] = temp;
+                } else continue;
+
+            }
+            len--;
         }
     }
 
